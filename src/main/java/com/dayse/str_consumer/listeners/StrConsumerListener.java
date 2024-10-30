8e49764ea0d6157c359c09db3e09294daf3a9e3a
Dayse Santos <daysedesign@gmail.com>
2024-10-30 11:02:14 -0300

    listener - logs por partição

--- src/main/java/com/dayse/str_consumer/listeners/StrConsumerListener.java
+++ src/main/java/com/dayse/str_consumer/listeners/StrConsumerListener.java
@@ -1,6 +1,7 @@
 package com.dayse.str_consumer.listeners;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j2;
@@ -9,9 +10,21 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StrConsumerListener {
     
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message){
-        log.info("Receive message {}", message);
+    @KafkaListener(groupId = "group-1", 
+    topicPartitions = {
+        @TopicPartition(topic = "str-topic", partitions = {"0"})
+    }, containerFactory = "strContainerFactory")
+    public void listenerLog0(String message){
+        log.info("LOG - Part 0 ::: Receive message {}", message);
     }
+
+    @KafkaListener(groupId = "group-1", 
+    topicPartitions = {
+        @TopicPartition(topic = "str-topic", partitions = {"1"})
+    }, containerFactory = "strContainerFactory")
+    public void listenerLog1(String message){
+        log.info("LOG - Part 1 ::: Receive message {}", message);
+    }
+
 
 }
